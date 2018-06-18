e336511643479b51ca29827ec7afe81c93d595f1
Tom <email@tomkent.me>
2018-06-18 16:03:17 +0100

    Fixed incorrect variable name

--- assets/js/game.js
+++ assets/js/game.js
@@ -50,7 +50,7 @@ $( document ).ready(function() {
 
     $.get('assets/revision', function(data) {
     	console.log(data);
-    	$('span.revision').text("-"+html.substring(1,8));
+    	$('span.revision').text("-"+data.substring(1,8));
     });
 
     jQuery.getJSON( "assets/changelogs/changelog.json", function( data ) {
